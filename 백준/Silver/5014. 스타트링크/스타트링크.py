09ccd6dad5c9c0cec4f8f893c7174591b6c7848f
kennedy0919 <139411494+kennedy0919@users.noreply.github.com>
2024-11-03 16:33:13 +0900

    [Silver I] Title: 스타트링크, Time: 200 ms, Memory: 127016 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -0,0 +1,27 @@
+# import sys
+from collections import deque
+
+# sys.stdin = open("input.txt", "r")
+
+def bfs(s, e):
+    q = deque()
+    visited = [0] * (F + 1)
+
+    q.append(s)
+    visited[s] = 1
+
+    while q:
+        c = q.popleft()
+        if c == e:
+            return visited[c] - 1
+        
+        for n in (c + U, c - D):
+            if 1 <= n <= F and visited[n] == 0:
+                q.append(n)
+                visited[n] = visited[c] + 1
+    return "use the stairs"
+
+F, S, G, U, D = map(int, input().split())
+
+ans = bfs(S, G)
+print(ans)
